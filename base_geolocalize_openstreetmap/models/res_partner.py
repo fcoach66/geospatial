b3283ec235d7c302756365be562e184ad072bb59
fcoach66 <fabrizio.cocciari@hotmail.it>
2025-01-13 05:38:30 +0100

    modified:   base_geolocalize_openstreetmap/models/res_partner.py

--- base_geolocalize_openstreetmap/models/res_partner.py
+++ base_geolocalize_openstreetmap/models/res_partner.py
@@ -29,7 +29,9 @@ class ResPartner(models.AbstractModel):
             "country": country or "",
         }
 
-        request_result = requests.get(cls._url, params=pay_load)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+
+        request_result = requests.get(cls._url, headers=headers, params=pay_load)
         try:
             request_result.raise_for_status()
         except Exception as e:
